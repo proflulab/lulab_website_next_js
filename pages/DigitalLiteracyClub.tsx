0381b7bc3eeac70f16425d6faa3efb698333972b
Yuyantao <2979706826@qq.com>
2024-08-24 16:06:02 +0800

    finish all modules of the clubs

--- pages/DigitalLiteracyClub.tsx
+++ pages/DigitalLiteracyClub.tsx
@@ -1,11 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
-// import { Grid, Typography } from "@mui/material";
 import ClubPageTemplate from "./ClubPageTemplate";
 import { LiteracyClubData } from "./clubInfo/LiteracyClubInfo";
 import "../app/globals.css";
-// import App from "../app/title/page";
-// import End from "../app/title/end";
+
 
 const LiteracyClub: React.FC = () => {
   return (
