b17870c13abfc3259a7b04fc970beb859273e785
Shenyinhang <Fiona.Shen0225@gmail.com>
2024-08-31 11:18:10 -0700

    仅保留course支付

--- .history/app/Clubs/Metaverse_20231203112107.tsx
+++
@@ -1,5 +0,0 @@
-import Link from "next/link";
-
-export default function Metaverse() {
-  return <Link href="/Metaverse">Metaverse Club</Link>;
-}
--- .history/app/title/Logo_20240101134605.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const Logo = () => (
-  <img src="/logo.png" alt="Logo" width="36" height="36" />
-);
-
-export default Logo;
