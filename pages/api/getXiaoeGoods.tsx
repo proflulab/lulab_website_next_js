80d23852c835182f128ef60abcc5b4ceb9cbdfc9
Shenyinhang <Fiona.Shen0225@gmail.com>
2024-09-02 11:05:40 -0700

    解决api问题

--- pages/api/getXiaoeGoods.tsx
+++ pages/api/getXiaoeGoods.tsx
@@ -4,7 +4,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "GET" && req.url?.includes("/getXiaoeToken")) {
+  if (req.method === "GET" && req.url?.includes("/pages/api/getXiaoeToken")) {
     console.log("Handling getXiaoeToken request...");
 
     const {
