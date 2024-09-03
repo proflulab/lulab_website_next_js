5a6a4f59085ed97f2a849af14981e74d0b694e93
杨仕明 <shiming.y@qq.com>
2024-09-03 08:07:58 +0800

    删除.history文件

--- .history/app/title/Logo_20240101134454.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Logo = () => <img src="/title.jpg" alt="Logo" width="36" height="36" />;
-
-export default Logo;
--- .history/pages/app_20231203160606.tsx
+++
@@ -1,2 +0,0 @@
-import "../app/globals.css";
-import "../app/layout.tsx";
