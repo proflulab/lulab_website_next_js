756eb0c6bb2494cd81d11e3de83166175783d2d0
杨仕明 <shiming.y@qq.com>
2024-09-08 02:01:45 +0800

    更新环境变量，更新课程数据请求方式

--- app/api/xiaoe/good/route.ts
+++ app/api/xiaoe/good/route.ts
@@ -2,62 +2,23 @@
  * @Author: 杨仕明 shiming.y@qq.com
  * @Date: 2024-09-07 00:58:59
  * @LastEditors: 杨仕明 shiming.y@qq.com
- * @LastEditTime: 2024-09-07 03:03:48
+ * @LastEditTime: 2024-09-08 01:58:48
  * @FilePath: /lulab_website_next_js-1/app/api/xiaoe/good/route.ts
  * @Description: 
  * 
  * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
  */
 
-import { revalidateTag } from "next/cache";
-import { getXiaoeToken } from "../../../../utils/xiaoetoken";
 
-const XIAOE_API_URL = "https://api.xiaoe-tech.com/xe.goods.detail.get/4.0.0";
-const RESOURCE_TYPE = 21;
-const RESOURCE_ID = "g_665f2719e0bcd_mrMARvTL54";
+import { getToken, goodsDetail } from "../../../../utils/xiaoe";
 
-async function fetchGoodsDetail(accessToken: string) {
-    const response = await fetch(XIAOE_API_URL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            access_token: accessToken,
-            resources: [
-                {
-                    "type": RESOURCE_TYPE,
-                    "ids": [RESOURCE_ID],
-                },
-            ],
-        }),
-        next: { revalidate: 24 * 60 * 60, tags: ["xiaoe-good"] }
-    });
 
-    if (!response.ok) {
-        throw new Error("Failed to fetch goods detail");
-    }
+const XIAOE_GOOD_TYPE = process.env.XIAOE_GOOD_TYPE;
+const XIAOE_GOOD_ID = process.env.XIAOE_GOOD_ID;
 
-    return response.json();
-}
 
 export async function GET(req: Request) {
-    try {
-        let tokenData = await getXiaoeToken();
-        let data = await fetchGoodsDetail(tokenData.data.access_token);
-
-        // 处理特定的错误码（例如 token 过期）
-        if (data.code === 2008) {
-            revalidateTag("xiaoe-token");
-            tokenData = await getXiaoeToken();
-            data = await fetchGoodsDetail(tokenData.data.access_token);
-        }
-
-        return Response.json(data);
-    } catch (error) {
-        // 更通用的错误处理方式
-        const errorMessage = error instanceof Error ? error.message : "Unknown error occurred";
-        console.error("Error fetching good:", errorMessage);
-        return Response.json({ message: "Internal server error" });
-    }
+    const resources = [{ "type": XIAOE_GOOD_TYPE, "ids": [XIAOE_GOOD_ID], },];
+    const good = await goodsDetail(resources)
+    return Response.json(good);
 }
--- pages/api/supabaseClient.tsx
+++ pages/api/supabaseClient.tsx
@@ -1,7 +1,18 @@
+/*
+ * @Author: 杨仕明 shiming.y@qq.com
+ * @Date: 2024-09-07 04:57:43
+ * @LastEditors: 杨仕明 shiming.y@qq.com
+ * @LastEditTime: 2024-09-08 01:07:35
+ * @FilePath: /lulab_website_next_js-1/pages/api/supabaseClient.tsx
+ * @Description: 
+ * 
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
+ */
+
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseKey = process.env.SUPABASE_ANON_KEY as string;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
