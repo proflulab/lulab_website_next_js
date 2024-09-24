3015ccc7fa22f5b373f0d9ebda1957df77316f32
Yuyantao <2979706826@qq.com>
2024-09-24 23:15:58 +0800

    add wechat official account code

--- components/bottombar/bottombar.tsx
+++ components/bottombar/bottombar.tsx
@@ -1,17 +1,29 @@
 'use client';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Typography, Grid, useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 
+const emailEnv = process.env.NEXT_PUBLIC_EMAIL as string;
 
 const BottomBar: React.FC = () => {
-    const email = process.env.REACT_APP_EMAIL;
+
+    // 使用useState设置初始值为空字符串
+    const [email, setEmail] = useState("");
+
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
     const t = useTranslations('BottomBar');
+
+    // 使用useEffect在组件挂载时读取环境变量
+    useEffect(() => {
+        if (emailEnv) {
+            setEmail(emailEnv); // 将环境变量的值设置到useState
+        }
+    }, []); // 空依赖数组，确保只在组件挂载时运行一次
+
 
     return (
         <Box
@@ -23,7 +35,7 @@ const BottomBar: React.FC = () => {
         >
             <Grid container alignItems="center" >
                 {/* Left section */}
-                <Grid item xs={12} sm={3} style={{ paddingLeft: isMobile ? '20px' : '100px', paddingTop: isMobile ? '30px' : '50px' }}>
+                <Grid item xs={12} sm={3} style={{ paddingLeft: isMobile ? '20px' : '100px', paddingTop: isMobile ? '30px' : '10px' }}>
                     <Box display="flex" alignItems="center">
                         <Image src="/images/logo.svg" alt="Lu Lab Logo" width={isMobile ? 50 : 60} height={isMobile ? 50 : 60} />
                         <Typography variant={isMobile ? "h5" : "h4"} sx={{ marginLeft: theme.spacing(2) }} style={{ fontWeight: 'bold' }}>
@@ -31,19 +43,26 @@ const BottomBar: React.FC = () => {
                         </Typography>
                     </Box>
                 </Grid>
-
                 {/* Middle section */}
-                <Grid item xs={12} sm={4} style={{ paddingLeft: isMobile ? '20px' : '30px', paddingTop: isMobile ? '10px' : '50px' }}>
+                <Grid item xs={12} sm={4} style={{ paddingLeft: isMobile ? '20px' : '10px', paddingTop: isMobile ? '10px' : '20px' }}>
                     <Box textAlign={'left'}>
                         <Typography variant="h6">{t('contact_info')}</Typography>
-                        <Typography variant="body2">{email}</Typography>
+                        <Typography variant="body2">{email ? email : "No email set"}</Typography>
                         <Typography variant="body2">
                             {t('contact_address')}
                         </Typography>
                     </Box>
                 </Grid>
                 {/* Right section */}
-                <Grid item xs={12} sm={3} style={{ paddingLeft: '20px', paddingTop: isMobile ? '10px' : '80px' }}>
+                <Grid item xs={12} sm={1.5} style={{ paddingLeft: '20px', paddingTop: isMobile ? '10px' : '70px' }}>
+                    <Box textAlign={'left'}>
+                        <Typography variant="body1">{t('wechat_official')}</Typography>
+                        <Box mt={1} >
+                            <Image src="/images/lu_official.jpg" alt="Lu Official account" width={80} height={80} />
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} sm={1.5} style={{ paddingLeft: '20px', paddingTop: isMobile ? '10px' : '70px' }}>
                     <Box textAlign={'left'}>
                         <Typography variant="body1">{t('scan')}</Typography>
                         <Box mt={1} >
@@ -52,15 +71,15 @@ const BottomBar: React.FC = () => {
                     </Box>
                 </Grid>
                 {/* Social media icons */}
-                <Grid item xs={12} sm={2} style={{ paddingLeft: '20px', paddingTop: isMobile ? '10px' : '30px', paddingBottom: isMobile ? '20px' : '0px' }}>
+                <Grid item xs={12} sm={2} style={{ paddingLeft: '20px', paddingTop: isMobile ? '10px' : '5px', paddingBottom: isMobile ? '20px' : '0px' }}>
                     <Typography variant="body1">{t('more_about')}</Typography>
                     <Box display="flex" textAlign={'left'} mt={2}>
                         <Link href="https://v.douyin.com/Ceir96WV/" passHref>
                             <Image src="/images/douyin-icon.jpg" alt="Douyin" width={24} height={24} style={{ marginRight: '8px' }} />
                         </Link>
-                        <Link href="https://space.bilibili.com/382176302" passHref>
+                        {/* <Link href="https://space.bilibili.com/382176302" passHref>
                             <Image src="/images/bilibili-icon.jpg" alt="Bilibili" width={24} height={24} style={{ marginRight: '8px' }} />
-                        </Link>
+                        </Link> */}
                         <Link href="https://www.xiaohongshu.com/user/profile/62dea0fe000000001f015e5a" passHref>
                             <Image src="/images/xiaohongshu-icon.jpg" alt="Xiaohongshu" width={24} height={24} />
                         </Link>
