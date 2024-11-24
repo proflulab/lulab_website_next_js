88974f98070f3d1721a02ec3da4a15679485ea73
杨仕明 <shiming.y@qq.com>
2024-11-24 12:38:30 +0800

    refactor(app): 优化商品数据接口定义和 VS Code 设置
    - 在 Course 组件中定义了 GoodsData 接口，用于规范商品数据结构
    - 在 VS Code 设置中添加了 Codegeex.RepoIndex 配置项
    - 优化了 XiaoeGood 路由中的 GET 函数定义

--- app/api/xiaoe/good/route.ts
+++ app/api/xiaoe/good/route.ts
@@ -2,7 +2,7 @@
  * @Author: 杨仕明 shiming.y@qq.com
  * @Date: 2024-09-07 00:58:59
  * @LastEditors: 杨仕明 shiming.y@qq.com
- * @LastEditTime: 2024-09-09 21:40:12
+ * @LastEditTime: 2024-11-24 12:34:13
  * @FilePath: /lulab_website_next_js/app/api/xiaoe/good/route.ts
  * @Description: 
  * 
@@ -17,7 +17,7 @@ const XIAOE_GOOD_TYPE = process.env.XIAOE_GOOD_TYPE;
 const XIAOE_GOOD_ID = process.env.XIAOE_GOOD_ID;
 
 
-export async function GET(req: Request) {
+export async function GET() {
     const resources = [{ "type": XIAOE_GOOD_TYPE, "ids": [XIAOE_GOOD_ID], },];
     const good = await goodsDetail(resources)
     return Response.json(good);
