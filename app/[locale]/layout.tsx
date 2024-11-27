0537495c480c621d88988f342b89475aeedba961
杨仕明 <shiming.y@qq.com>
2024-11-27 22:25:14 +0800

    refactor(layout): 优化页面布局和导航栏样式
    - 调整 RootLayout 组件结构，使用 flex 布局
    - 修改 Navbar 组件样式，设置固定高度和背景颜色
    - 移除导航栏的模糊效果，提高性能
    - 调整导航栏容器样式，优化对齐

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,7 +2,7 @@
  * @Author: 杨仕明 shiming.y@qq.com
  * @Date: 2024-09-08 03:01:48
  * @LastEditors: 杨仕明 shiming.y@qq.com
- * @LastEditTime: 2024-11-27 21:32:07
+ * @LastEditTime: 2024-11-27 22:12:28
  * @FilePath: /lulab_website_next_js/app/[locale]/layout.tsx
  * @Description:
  *
@@ -11,7 +11,6 @@
 
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
-
 import React from 'react';
 import "./globals.css";
 import Navbar from '@/components/navbar/navbar';
@@ -30,16 +29,17 @@ export default async function RootLayout({
 
   return (
     <html lang={locale}>
-      <head>
-      </head>
-      <body><NextIntlClientProvider messages={messages}>
-        <React.StrictMode>
-          <Navbar />
-        </React.StrictMode>
-        <main style={{ paddingTop: '70px' }}>
-          {children}
-        </main>
-      </NextIntlClientProvider>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          <div className="flex flex-col min-h-screen">
+            <React.StrictMode>
+              <Navbar />
+            </React.StrictMode>
+            <main>
+              {children}
+            </main>
+          </div>
+        </NextIntlClientProvider>
       </body>
     </html>
   )
