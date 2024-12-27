26ec39f1b1677931583e38764847afc38fee41bd
Shenyinhang <Fiona.Shen0225@gmail.com>
2024-12-27 15:42:45 -0800

    修复错误

--- app/api/xiaoe/good/route.ts
+++ app/api/xiaoe/good/route.ts
@@ -1,24 +1,21 @@
-/*
- * @Author: 杨仕明 shiming.y@qq.com
- * @Date: 2024-09-07 00:58:59
- * @LastEditors: 杨仕明 shiming.y@qq.com
- * @LastEditTime: 2024-11-24 12:34:13
- * @FilePath: /lulab_website_next_js/app/api/xiaoe/good/route.ts
- * @Description: 
- * 
- * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
- */
+// /*
+//  * @Author: 杨仕明 shiming.y@qq.com
+//  * @Date: 2024-09-07 00:58:59
+//  * @LastEditors: 杨仕明 shiming.y@qq.com
+//  * @LastEditTime: 2024-11-24 12:34:13
+//  * @FilePath: /lulab_website_next_js/app/api/xiaoe/good/route.ts
+//  * @Description:
+//  *
+//  * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved.
+//  */
 
+// import { goodsDetail } from "../../../../utils/xiaoe";
 
-import { goodsDetail } from "../../../../utils/xiaoe";
+// const XIAOE_GOOD_TYPE = process.env.XIAOE_GOOD_TYPE;
+// const XIAOE_GOOD_ID = process.env.XIAOE_GOOD_ID;
 
-
-const XIAOE_GOOD_TYPE = process.env.XIAOE_GOOD_TYPE;
-const XIAOE_GOOD_ID = process.env.XIAOE_GOOD_ID;
-
-
-export async function GET() {
-    const resources = [{ "type": XIAOE_GOOD_TYPE, "ids": [XIAOE_GOOD_ID], },];
-    const good = await goodsDetail(resources)
-    return Response.json(good);
-}
+// export async function GET() {
+//     const resources = [{ "type": XIAOE_GOOD_TYPE, "ids": [XIAOE_GOOD_ID], },];
+//     const good = await goodsDetail(resources)
+//     return Response.json(good);
+// }
