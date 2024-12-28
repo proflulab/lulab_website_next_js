0fd91b6b2809619ad1720b70a2f7c6a341d3bf47
杨仕明 <shiming.y@qq.com>
2024-12-29 04:55:24 +0800

    refactor(i18n): 重构国际化配置并优化多语言支持
    - 更新了多语言配置文件结构，使其更加规范化和易于管理
    - 重构了国际化路由和请求处理逻辑，提高了性能和可维护性
    - 优化了组件中的国际化使用方式，提升了开发体验
    - 删除了未使用的旧代码文件，精简了项目结构

--- i18n/request.ts
+++ i18n/request.ts
@@ -2,23 +2,27 @@
  * @Author: 杨仕明 shiming.y@qq.com
  * @Date: 2024-09-10 21:18:01
  * @LastEditors: 杨仕明 shiming.y@qq.com
- * @LastEditTime: 2024-09-10 22:36:42
+ * @LastEditTime: 2024-12-28 19:33:18
  * @FilePath: /lulab_website_next_js/i18n/request.ts
- * @Description: 
- * 
- * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
+ * @Description:
+ *
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved.
  */
 
+import { getRequestConfig } from "next-intl/server";
+import { routing } from "./routing";
 
-import { notFound } from 'next/navigation';
-import { getRequestConfig } from 'next-intl/server';
-import { routing } from './routing';
+export default getRequestConfig(async ({ requestLocale }) => {
+  // This typically corresponds to the `[locale]` segment
+  let locale = await requestLocale;
 
-export default getRequestConfig(async ({ locale }) => {
-    // Validate that the incoming `locale` parameter is valid
-    if (!routing.locales.includes(locale as any)) notFound();
+  // Ensure that a valid locale is used
+  if (!locale || !routing.locales.includes(locale as any)) {
+    locale = routing.defaultLocale;
+  }
 
-    return {
-        messages: (await import(`../messages/${locale}.json`)).default
-    };
+  return {
+    locale,
+    messages: (await import(`../messages/${locale}.json`)).default,
+  };
 });
