ca77e310ff264f91c86dcb369dbcaf2123453fe6
杨仕明 <shiming.y@qq.com>
2025-01-06 00:20:15 +0800

    refactor(layout): 注释掉导航栏和页脚组件
    移除了 RootLayout 组件中的 Navbar 和 Footer 组件的引用和使用。这两个组件现在被注释掉，以便未来可能的重新引入。

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,7 +2,7 @@
  * @Author: 杨仕明 shiming.y@qq.com
  * @Date: 2024-09-08 03:01:48
  * @LastEditors: 杨仕明 shiming.y@qq.com
- * @LastEditTime: 2025-01-05 19:52:39
+ * @LastEditTime: 2025-01-06 00:20:06
  * @FilePath: /lulab_website_next_js/app/[locale]/layout.tsx
  * @Description:
  *
@@ -13,8 +13,6 @@ import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import React from 'react';
 import "./globals.css";
-import Navbar from '@/components/navbar/navbar';
-import { Footer } from '@/components/footer';
 
 export const metadata = {
   title: 'Lu Lab',
@@ -40,11 +38,9 @@ export default async function RootLayout({
       <body>
         <NextIntlClientProvider messages={messages}>
           <div className="flex flex-col min-h-screen">
-            {/* <Navbar /> */}
             <main>
               {children}
             </main>
-            {/* <Footer /> */}
           </div>
         </NextIntlClientProvider>
       </body>
