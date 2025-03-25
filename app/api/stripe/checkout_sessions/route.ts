93c8e9a3f06c01cf84c46fa19c3a4412e9547aae
杨仕明 <shiming.y@qq.com>
2025-03-25 12:10:11 +0800

    feat(stripe): update checkout session to use env variable and enable phone collection
    Update the Stripe checkout session to use the price ID from an environment variable instead of a hardcoded value. Additionally, enable phone number collection during the checkout process.

--- app/api/stripe/checkout_sessions/route.ts
+++ app/api/stripe/checkout_sessions/route.ts
@@ -2,7 +2,7 @@
  * @Author: 杨仕明 shiming.y@qq.com
  * @Date: 2025-01-05 17:10:51
  * @LastEditors: 杨仕明 shiming.y@qq.com
- * @LastEditTime: 2025-03-23 23:55:43
+ * @LastEditTime: 2025-03-25 12:08:36
  * @FilePath: /lulab_website_next_js/app/api/stripe/checkout_sessions/route.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -19,10 +19,13 @@ export async function POST(request: Request) {
       ui_mode: 'embedded',
       line_items: [
         {
-          price: 'price_1PE0BAJAR8bDRsEHP4N6VElg',
+          price: process.env.STRIPE_PRICE_ID, // 从环境变量读取价格ID
           quantity: 1,
         },
       ],
+      phone_number_collection: {
+        enabled: true,
+      },
       mode: 'payment',
       return_url: `${request.headers.get('origin')}/return?session_id={CHECKOUT_SESSION_ID}`,
     });
