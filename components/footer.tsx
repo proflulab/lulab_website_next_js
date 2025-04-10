aaab83dba20fcf59d98cde3e8b96832d3b56656a
renjismzy <renjismz@gmail.com>
2025-04-10 21:20:29 +0800

    修改二维码

--- components/footer.tsx
+++ components/footer.tsx
@@ -92,31 +92,33 @@ export function Footer() {
 
                     {/* Follow Us Section */}
                     <FooterColumn title={t('followUs.title')}>
-                        {/* <p className="text-muted-foreground">
-                            {t('followUs.items.wechat')}
-                        </p> */}
-                        <p className="text-muted-foreground">
-                            <HoverCard>
-                                <HoverCardTrigger asChild>
-                                    {/* <Button variant="link">{t('followUs.items.wechat')}</Button> */}
-                                    {/* {t('followUs.items.wechat')} */}
-                                    <span className="cursor-pointer">
-                                        {t('followUs.items.wechat')}
-                                    </span>
-                                </HoverCardTrigger>
-                                <HoverCardContent className="p-0 w-auto h-auto">
-                                    <div className="flex justify-center items-center">
-                                        <Image
-                                            src="/images/about/wechat-qr.jpg"
-                                            alt="WeChat QR Code"
-                                            width={200}
-                                            height={200}
-                                            className="rounded-lg"
-                                        />
-                                    </div>
-                                </HoverCardContent>
-                            </HoverCard>
-                        </p>
+                        <div className="flex flex-col -mt-4 space-y-3">
+                            <div className="flex gap-3 justify-start">
+                                {/* 公众号二维码 */}
+                                <div className="border border-gray-200 p-1.5 rounded-md">
+                                    <Image
+                                        src="/images/about/wechat-qr.jpg"
+                                        alt="WeChat QR Code"
+                                        width={96}
+                                        height={96}
+                                        className="rounded-sm"
+                                    />
+                                    <p className="text-center text-xs text-gray-600 mt-1.5">扫码关注微信公众号</p>
+                                </div>
+                                
+                                {/* 小程序二维码 */}
+                                <div className="border border-gray-200 p-1.5 rounded-md">
+                                    <Image
+                                        src="/images/about/qywork.jpg"
+                                        alt="Mini Program QR Code"
+                                        width={96}
+                                        height={96}
+                                        className="rounded-sm"
+                                    />
+                                    <p className="text-center text-xs text-gray-600 mt-1.5">扫码联系我们</p>
+                                </div>
+                            </div>
+                        </div>
                     </FooterColumn>
                 </div>
 
